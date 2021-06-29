cf93091f119274051257143410f2faba72f6bf37
Emanoel Cardoso <emanoel97@hotmail.com>
2021-06-29 16:49:31 -0300

    quick fick margin em servicos

--- src/components/Servicos.vue
+++ src/components/Servicos.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div>
-            <div class="relative flex justify-center gap-14 z-100 -top-20">
+            <div class="relative flex justify-center gap-14 z-100 -top-20 -mb-12">
                 <div class="py-6 flex sm:py-12">
                     <div class="relative py-3 sm:max-w-sm sm:mx-auto">
                         <div class="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>

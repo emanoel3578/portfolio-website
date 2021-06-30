4bce1484f8180458e680f3311fa85d41efaa9493
Emanoel Cardoso <emanoel97@hotmail.com>
2021-06-30 12:00:50 -0300

    Atualizado novas imagens em projeto, e correção em algumas fontes

--- src/components/Projetos.vue
+++ src/components/Projetos.vue
@@ -25,19 +25,26 @@
             <div v-if="sites" class="">
                 <div class="flex w-3/5 mx-auto my-0 items-center mb-5">
                     <div class="flex rounded-full bg-blue-500 w-full">
-                        <img class="p-5" src="img/computer.png">
+                        <img class="p-5 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110" src="img/computer.png">
                     </div>
                     
                     <div class="flex flex-col">
-                        <div class="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
-                            <div class="text-center font-bold text-3xl mb-2">
-                                <p class="font-kanit font-bold">Front Finanças</p>
+                        <div class="flex justify-center transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mb-4">
+                            <div class="text-center text-3xl">
+                                <p class="font-kanit text-blue-900 font-semibold">Front Finanças - </p>
+                            </div>
+
+                            <div class="flex justify-center ml-2">
+                                <p class="p-2 font-kanit rounded-3xl bg-gray-800 text-white border-3 border-gray-100 bg-gradient-to-r hover:from-blue-500 hover:to-purple-400 hover:text-white cursor-pointer shadow-lg">
+                                    Visitar projeto
+                                </p>
                             </div>
                         </div>
 
-                        <div class="text-center">
+                        <div class="text-center ml-9">
                             <p class="font-poppins font-semibold transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">Laborum cupidatat in aute enim laboris occaecat. Eu nulla adipisicing aliqua dolor occaecat. Qui magna minim cupidatat aliqua sint sunt deserunt consequat est fugiat. Amet anim Lorem culpa sit Lorem pariatur ea ullamco. Sit Lorem ullamco voluptate eiusmod aliqua officia do occaecat cupidatat ad fugiat. Veniam reprehenderit dolore exercitation voluptate dolor in deserunt ad dolor velit ex. Nostrud incididunt eu est exercitation aute do cillum.</p>
                         </div>
+
                     </div>
                 </div>
             
@@ -45,17 +52,23 @@
             
                 <div class="flex w-3/5 mx-auto my-0  items-center mb-5">
                     <div class="flex rounded-full bg-blue-500 w-full">
-                        <img class="p-5" src="img/computer.png">
+                        <img class="p-5 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110" src="img/computer2.png">
                     </div>
                     
                     <div class="flex flex-col">
-                        <div class="">
-                            <div class="text-center font-bold text-3xl mb-2">
-                                <p class="font-kanit font-bold transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">Front Finanças</p>
+                        <div class="flex justify-center transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mb-5">
+                            <div class="text-center text-3xl">
+                                <p class="font-kanit text-blue-900 font-semibold ">Galaxy Studios -</p>
+                            </div>
+
+                            <div class="flex justify-center ml-2">
+                                <p class="p-2 font-kanit rounded-3xl bg-gray-800 text-white border-3 border-gray-100 bg-gradient-to-r hover:from-blue-500 hover:to-purple-400 hover:text-white cursor-pointer shadow-lg">
+                                    Visitar projeto
+                                </p>
                             </div>
                         </div>
 
-                        <div class="text-center">
+                        <div class="text-center ml-9">
                             <p class="font-poppins font-semibold transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">Laborum cupidatat in aute enim laboris occaecat. Eu nulla adipisicing aliqua dolor occaecat. Qui magna minim cupidatat aliqua sint sunt deserunt consequat est fugiat. Amet anim Lorem culpa sit Lorem pariatur ea ullamco. Sit Lorem ullamco voluptate eiusmod aliqua officia do occaecat cupidatat ad fugiat. Veniam reprehenderit dolore exercitation voluptate dolor in deserunt ad dolor velit ex. Nostrud incididunt eu est exercitation aute do cillum.</p>
                         </div>
                     </div>
@@ -67,22 +80,28 @@
             <div v-if="aplicativos">
                 <div class="flex w-3/5 mx-auto my-0  items-center mb-5">
                     <div class="flex rounded-full bg-blue-500 w-full items-center">
-                        <img class="mx-auto my-0" src="img/computer.png">
+                        <img class="mx-auto my-0 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110" src="img/computer.png">
                     </div>
                     
                     <div class="flex flex-col">
-                        <div class="">
-                            <div class="text-center font-bold text-3xl mb-2">
-                                <p class="font-semibold font-kanit transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">Front Finanças APP</p>
+                        <div class="flex justify-center transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 mb-5">
+                            <div class="text-center text-3xl mb-2">
+                                <p class="font-semibold font-kanit transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 text-blue-900 font-semibold">Front Finanças APP -</p>
+                            </div>
+
+                            <div class="flex justify-center ml-2">
+                                <p class="p-2 font-kanit rounded-3xl bg-gray-800 text-white border-3 border-gray-100 bg-gradient-to-r hover:from-blue-500 hover:to-purple-400 hover:text-white cursor-pointer shadow-lg">
+                                    Visitar projeto
+                                </p>
                             </div>
                         </div>
 
-                        <div class="text-center">
+                        <div class="text-center mx-8">
                             <p class="font-kanit transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">Laborum cupidatat in aute enim laboris occaecat. Eu nulla adipisicing aliqua dolor occaecat. Qui magna minim cupidatat aliqua sint sunt deserunt consequat est fugiat. Amet anim Lorem culpa sit Lorem pariatur ea ullamco. Sit Lorem ullamco voluptate eiusmod aliqua officia do occaecat cupidatat ad fugiat. Veniam reprehenderit dolore exercitation voluptate dolor in deserunt ad dolor velit ex. Nostrud incididunt eu est exercitation aute do cillum.</p>
                         </div>
                     </div>
-                    <div class="flex bg-blue-500 rounded-full py-3 px-6 w-full">
-                        <img class="" src="img/phone.png">
+                    <div class="flex bg-blue-500 rounded-full w-full">
+                        <img src="img/phoneframe.png" class="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
                     </div>
                 </div>
             </div>

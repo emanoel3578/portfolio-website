bf1c6bde0d5c04ecd9bfcb76ae09ca69b560e28e
Emanoel Cardoso <emanoel97@hotmail.com>
2021-07-02 11:49:25 -0300

    commit para nova aba servico, porem ainda vai sofrer mudancas

--- src/components/Servicos.vue
+++ src/components/Servicos.vue
@@ -43,8 +43,49 @@
             </div>
         </div>
 
-        <div id="servicosID" class="flex flex-col md:flex md:flex-row bg-gray-100 justify-center w-full">
-            <div class="flex flex-col md:flex md:flex-row md:w-3/5 gap-6 items-center ">
+        <div id="servicosID" class="flex flex-col md:flex md:flex-row mx-auto my-0 w-full">
+            <div class="flex flex-col">
+                <div class="flex items-center bg-blue-500 slide-in-left">
+                    <div class="w-1/2" id="office1Div">
+                    </div>
+                    <div class="flex flex-col w-1/2  text-center font-kanit text-gray-100 px-2">
+                        <p class="font-bold text-4xl">Criação de Sistemas e sites que cabem no seu bolso</p>
+                        <p class="text-2xl">Temos os melhores valores para o seu negócio crescer com a ajuda da tecnologia, nos orgulhamos em oferecer um serviço de qualidade e preços que buscam caber dentro do orçamento de negócios de pequenas e médias empresas.</p>
+                    </div>
+                </div>
+
+                <div class="flex items-center slide-in-right">
+                    <div class="flex flex-col w-1/2  text-center font-kanit text-black px-2">
+                        <p class="font-bold text-4xl">Desenvolvimento de Sites para todas as plataformas</p>
+                        <p class="text-2xl">Desenvolvemos todos os tipos de sites, do mais simples ao mais complexo sempre buscando o equilíbrio entre desempenho e design para que o usuário tenha a melhor experiència possível.</p>
+                    </div>
+                    <div class="w-1/2" id="office2Div">
+                    </div>
+                </div>
+
+               <div class="flex items-center bg-blue-500 slide-in-left">
+                    <div class="w-1/2" id="office3Div">
+                    </div>
+                    <div class="flex flex-col w-1/2  text-center font-kanit text-gray-100 px-2">
+                        <p class="font-bold text-4xl">A qualquer momento e em qualquer lugar seu negócio está online</p>
+                        <p class="text-2xl">Todo projeto que desenvolvemos vem com a garantia que você podera acessar em qualquer aparelho que tenha acesso a internet, em qualquer lugar a qualquer momento.</p>
+                    </div>
+                </div>
+
+                <div class="flex items-center slide-in-right">
+                    <div class="flex flex-col w-1/2  text-center font-kanit text-black px-2">
+                        <p class="font-bold text-4xl">Suporte 24hrs 7dias da semana, você nunca está sozinho</p>
+                        <p class="text-2xl">Garantimos suporte a todo momento sempre junto de você, queremos te mostrar como é fácil atravez da nossa plataforma solucionar qualquer problema.</p>
+                    </div>
+                    <div class="w-1/2" id="office4Div">
+                    </div>
+                </div>
+            </div>
+
+
+
+
+            <!-- <div class="flex flex-col md:flex md:flex-row md:w-3/5 gap-6 items-center ">
                 <div class="text-center relative">
                     <p class="text-5xl font-kanit pt-4 md:pt-0 md:font-semibold">Serviços</p>
                 </div>
@@ -79,7 +120,7 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> -->
         </div>
     </div>
 </template>
@@ -141,6 +182,40 @@ export default {
     -webkit-transform: scale(1);
             transform: scale(1);
   }
+}
+
+#office1Div {
+    background:url('../../public/img/office1.png');
+    background-size: 100% 100%;
+    height: 55vh;
+}
+
+#office2Div {
+    background:url('../../public/img/office2.jpg');
+    background-size: 100% 100%;
+    height: 55vh;
+}
+
+#office3Div {
+    background:url('../../public/img/office3.jpg');
+    background-size: 100% 100%;
+    height: 55vh;
+}
+
+#office4Div {
+    background:url('../../public/img/office4.jpg');
+    background-size: 100% 100%;
+    height: 55vh;
+}
+
+.slide-in-left {
+	-webkit-animation: slide-in-left 2s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
+    animation: slide-in-left 2s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
+}
+
+.slide-in-right {
+	-webkit-animation: slide-in-right 2s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
+    animation: slide-in-right 2s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
 }
 
 section {
@@ -208,5 +283,55 @@ section .wave.wave4 {
     100% {
         background-position-x: 1000px;
     }
+}
+
+@-webkit-keyframes slide-in-left {
+  0% {
+    -webkit-transform: translateX(-1000px);
+            transform: translateX(-1000px);
+    opacity: 0;
+  }
+  100% {
+    -webkit-transform: translateX(0);
+            transform: translateX(0);
+    opacity: 1;
+  }
+}
+@keyframes slide-in-left {
+  0% {
+    -webkit-transform: translateX(-1000px);
+            transform: translateX(-1000px);
+    opacity: 0;
+  }
+  100% {
+    -webkit-transform: translateX(0);
+            transform: translateX(0);
+    opacity: 1;
+  }
+}
+
+@-webkit-keyframes slide-in-right {
+  0% {
+    -webkit-transform: translateX(1000px);
+            transform: translateX(1000px);
+    opacity: 0;
+  }
+  100% {
+    -webkit-transform: translateX(0);
+            transform: translateX(0);
+    opacity: 1;
+  }
+}
+@keyframes slide-in-right {
+  0% {
+    -webkit-transform: translateX(1000px);
+            transform: translateX(1000px);
+    opacity: 0;
+  }
+  100% {
+    -webkit-transform: translateX(0);
+            transform: translateX(0);
+    opacity: 1;
+  }
 }
 </style>

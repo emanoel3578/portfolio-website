63489f7559e4ffbc2fb2a3c9111ee3f796ca1ff1
Emanoel Cardoso <emanoel97@hotmail.com>
2021-07-02 02:44:56 -0300

    correções de text e padding em servicos, possivel mudanca no futuro ?

--- src/components/Servicos.vue
+++ src/components/Servicos.vue
@@ -44,8 +44,8 @@
         </div>
 
         <div id="servicosID" class="flex flex-col md:flex md:flex-row bg-gray-100 justify-center w-full">
-            <div class="flex flex-col md:flex md:flex-row md:w-3/5 gap-6 items-center">
-                <div class="text-center">
+            <div class="flex flex-col md:flex md:flex-row md:w-3/5 gap-6 items-center ">
+                <div class="text-center relative">
                     <p class="text-5xl font-kanit pt-4 md:pt-0 md:font-semibold">Serviços</p>
                 </div>
 
@@ -143,6 +143,70 @@ export default {
   }
 }
 
+section {
+    position:absolute;
+    width:100%;
+    background: #3586ff;
+    overflow:hidden;
+}
 
+section .wave {
+    position:absolute;
+    bottom:0;
+    left:0;
+    width:100%;
+    height:100px;
+    background:url('../../public/img/wave.png');
+    background-size: 1000px 100px;
+}
 
+section .wave.wave1 {
+    animation: animate 30s linear infinite;
+    z-index:1000;
+    opacity:1;
+    animation-delay: 0s;
+    bottom:0;
+}
+
+section .wave.wave2 {
+    animation: animate2 15s linear infinite;
+    z-index:999;
+    opacity: 0.5;
+    animation-delay: -5s;
+    bottom: 10px;
+}
+
+section .wave.wave3 {
+    animation: animate 30s linear infinite;
+    z-index:998;
+    opacity: 0.2;
+    animation-delay: -2s;
+    bottom: 15;
+}
+
+section .wave.wave4 {
+    animation: animate2 5s linear infinite;
+    z-index:997;
+    opacity: 0.7;
+    animation-delay: -2s;
+    bottom: 20px;
+}
+
+@keyframes animate {
+    0% {
+        background-position-x: 0;
+    }
+    100% {
+        background-position-x: 1000px;
+    }
+}
+
+@keyframes animate2 {
+    0% {
+        background-position-x: 0;
+    }
+    100% {
+        background-position-x: 1000px;
+    }
+}
 </style>
